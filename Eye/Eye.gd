6fca2df4085f903503c7328a0d244d9d5a8a2672
gilzoide <gilzoide@gmail.com>
2020-08-03 14:44:59 -0300

    Smooth eyes' look_at

--- Eye/Eye.gd
+++ Eye/Eye.gd
@@ -3,6 +3,15 @@ extends Spatial
 export(NodePath) var pointerPath
 onready var pointer: Spatial = get_node(pointerPath)
 
-func _process(_delta):
+export(float) var look_speed = 4
+var looking_at: Vector3
+
+func _init():
 	if pointer:
-		look_at(pointer.global_transform.origin, Vector3.DOWN)
+		looking_at = pointer.global_transform.origin
+
+func _process(delta):
+	if pointer:
+		var pointer_pos = pointer.global_transform.origin
+		looking_at = looking_at.linear_interpolate(pointer_pos, delta * look_speed)
+		look_at(looking_at, Vector3.DOWN)
